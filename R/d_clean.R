b044c090bd95730e3b85124793e6100b9c4c1739
decision-sciences <decision_sciences@columbia.edu>
2016-08-24 13:16:54 -0400

    Update d_clean.R

--- R/d_clean.R
+++ R/d_clean.R
@@ -15,51 +15,31 @@
 
 d_clean <- function(df, var, file_type = "csv"){
 
-  # make sure factors are correctly converted into numeric format
-  # used in [selectcol] function
-
-
-  # for .csv, use the identified variable as unique_id and remove the first row
-
+  # for .csv, use the identified variable as unique_ID and remove the first row (variable name,)
   uniqR_csv <- function(df, var) {
-    df <- df %>% rename_(unique_id = as.symbol(var))
+    df <- df %>% rename_(unique_ID = as.symbol(var))
     df <- df[-1, ]
+    return(df)
   }
-
-  # for .xml, use the identified variable as unique_id
-
+  # for .xml, use the identified variable as unique_ID
   uniqR_xml <- function(df, var){
-    df <- df %>% rename_(unique_id = as.symbol(var))
+    df <- df %>% rename_(unique_ID = as.symbol(var))
+    return(df)
   }
-
-  # take dataframe and subset only the columns of responses
-  # to risk- questions and make sure that they are in numeric
-  # format. Then, link unique_id and responses back as identifier.
-
-  # df should be contain only rows of responses
-  # column names of risk-responses should start with first three
-  # characters of the domain followed by RT, RB, RP
-  # df should have a column named 'unique_id', uniquely identifying the
-  # survey taker.
-
-
-  # full.panel separates benefit/taking/perception questions and
-  # merge them by unique_id, domain and question number
-  # and add a numeric id by unique_id
 
   full.panel <- function(df){
 
-    ffac_friendly <- function(x) {
+    fac_friendly <- function(x) {
       x <- as.numeric(as.character(x))
     }
     selectcol <- function(df){
-      
+
       pat = "[a-z]{3}[A-Z]{2}_[0-9]{1}|unique_ID"
       df <- df[ , grepl( pat, colnames(df))]
-      
+
       df[ , !(colnames(df) == "unique_ID")] <- df %>%
         select(-unique_ID) %>% mutate_each(funs(fac_friendly))
-      
+
       ## making sure correct type of conversion
       return(df)
     }
@@ -68,45 +48,42 @@ d_clean <- function(df, var, file_type = "csv"){
     # changes input df into longform and identifies the question
     # domain, number and type (e.g. fin 6 RT, 6th question in
     # finance domain, of Risk Taking)
-    panelform <- function(df, var) {
-      df <- df %>% select( unique_id, contains(paste0(var, "_")))
-      df <- melt(df, id="unique_id", value.name = var)
-      df <- mutate(df, domain = substr(variable, 1, 3), Qnumber=substr(variable, 7, 7))
-      df <- select(df, -variable)
+    panelform <- function(df) {
+      wide <- reshape2::melt(df, id.vars = "unique_ID")
+      wide$domain <- substr(wide$variable, 1, 3)
+      wide$Qnumber <- substr(wide$variable, 7, 7)
+      wide$type <- substr(wide$variable, 4, 5)
+
+      wide_RB <- wide %>% filter(type == "RB") %>% select(-variable, -type) %>%
+        rename(RB = value)
+      wide_RP <- wide %>% filter(type == "RP") %>% select(-variable, -type) %>%
+        rename(RP = value)
+      wide_RT <- wide %>% filter(type == "RT") %>% select(-variable, -type) %>%
+        rename(RT = value)
+
+      l = list(wide_RB, wide_RT, wide_RP)
+      df <- join_all(l, type = "full") %>% select(unique_ID, domain, Qnumber, RB, RP, RT)
+      rm(wide)
+
+      df <- df %>% arrange(unique_ID, domain, Qnumber)
+
       return(df)
     }
 
-    df <- selectcol(df)
-    benefit <- panelform(df, "RB")
-    taking <- panelform(df, "RT")
-    perception <- panelform(df, "RP")
-
-    df <- Reduce(function(x,y) merge(x, y, by=c("unique_id", "domain", "Qnumber"),
-                                     all=TRUE), list(taking, perception, benefit))
-    df <- mutate(df, id=as.numeric(factor(unique_id)))
+    df <- panelform(selectcol(df))
+    # df <- mutate(df, id=as.numeric(factor(unique_ID)))
     return(df)
   }
 
 
-  df <- dplyr:: tbl_df(df)
-
   if (file_type == "csv") {
-    df <- uniqR_csv(df, var)
-    dat <- full.panel(df)
-    dat$sum <- rowSums(cbind(dat$RT, dat$RP, dat$RB))
+    df <- full.panel(uniqR_csv(df, var))
+    return(df)
 
-    dat_group <- dat %>% group_by(id) %>% dplyr::mutate(group_sum = sum(sum)) %>%
-      filter(!is.na(group_sum)) %>% select(-sum, -group_sum)
-    dat <- ungroup(dat_group)
-    return(dat)
   } else if (file_type == "xml") {
-    df <- uniqR_xml(df, var)
-    dat <- full.panel(df)
-    dat$sum <- rowSums(cbind(dat$RT, dat$RP, dat$RB))
-    dat_group <- dat %>% group_by(id) %>% dplyr::mutate(group_sum = sum(sum)) %>%
-      filter(!is.na(group_sum)) %>% select(-sum, -group_sum)
-    dat <- ungroup(dat_group)
-    return(dat)
+    df <- full.panel(uniqR_xml(df, var))
+    return(df)
+
   }
 }
 
